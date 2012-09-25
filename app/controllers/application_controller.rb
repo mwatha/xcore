8e5acbaf9ee5e52990f45c1692d487422ca28d8c
Mwatha Bwanali <mwathabwanali@gmail.com>
2012-09-26 00:48:51 +0200

    working on the UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :perform_basic_auth, :only => :login
+  before_filter :perform_basic_auth, :except => :login
 
   protected
 
   def perform_basic_auth
-    if not (session[:user_id].blank?)
+    if session[:user_id].blank?
       respond_to do |format|                                                      
         format.html { redirect_to :controller => 'users',:action => 'login' }    
       end
