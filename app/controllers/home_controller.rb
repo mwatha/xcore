ce58ad7aebc43661d476a04f409126ab833a2990
Mwatha Bwanali <mwathabwanali@gmail.com>
2012-09-30 14:12:11 +0200

    working on the UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @total_registered_students = People.joins(:people_names,                                     
+      :people_attributes).where(:'people_attributes.value' => 'Student').select("people.*,
+        people_attributes.*,people_names.*").count
+    @total_available_programs = Programs.count
+    @total_available_staff =  People.joins(:people_names,                    
+      :people_attributes).where(:'people_attributes.value' => 'Staff').select("people.*,
+        people_attributes.*,people_names.*").count 
   end
 
 end
